98e61073577328a93b9837973d27bc650e38e343
Angel Hernandez <angel@magic.link>
2023-07-01 13:50:06 -0400

    add Header and Footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div id="footerInfo">
+        <a href="https://magic.link/docs/home/welcome">Magic SDK Docs</a>
+      </div>
+    </footer>
+  );
+}
